ad73174b044be7f66c1b723903745dd523d0a01a
Jean Paulo <jean2paulo@gmail.com>
2024-06-13 21:35:01 -0300

    feature: adding playlist on menu

--- app/src/main/java/com/jeanpaulo/musiclibrary/app/ui/components/BottomNavigationComponent.kt
+++ app/src/main/java/com/jeanpaulo/musiclibrary/app/ui/components/BottomNavigationComponent.kt
@@ -3,6 +3,7 @@ package com.jeanpaulo.musiclibrary.app.ui.components
 import android.content.res.Configuration
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Build
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -31,7 +32,8 @@ fun BottomNavigationBar(navController: NavController) {
     val items = listOf(
         NavigationItem.Home,
         NavigationItem.Search,
-        NavigationItem.Config
+        NavigationItem.Playlist,
+        NavigationItem.Config,
     )
 
     NavigationBar(
@@ -118,5 +120,6 @@ fun Navigation(navController: NavHostController) {
 sealed class NavigationItem(val route: String, val icon: ImageVector, val label: String) {
     object Home : NavigationItem("home", Icons.Default.Home, "Home")
     object Search : NavigationItem("search", Icons.Default.Search, "Search")
+    object Playlist : NavigationItem("playlists", Icons.Default.Favorite, "Playlists")
     object Config : NavigationItem("config", Icons.Default.Build, "Config")
 }
