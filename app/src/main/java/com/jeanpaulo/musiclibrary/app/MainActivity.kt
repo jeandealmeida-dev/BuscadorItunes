316404dafc7096454114a0cca71422c4c3f69963
Jean Paulo <jean2paulo@gmail.com>
2024-07-13 19:58:35 -0300

    refactoring: removing old references music detail

--- app/src/main/java/com/jeanpaulo/musiclibrary/app/MainActivity.kt
+++ app/src/main/java/com/jeanpaulo/musiclibrary/app/MainActivity.kt
@@ -3,10 +3,6 @@ package com.jeanpaulo.musiclibrary.app
 import android.content.Intent
 import android.content.IntentFilter
 import android.os.Bundle
-import android.util.Log
-import android.view.ContextMenu
-import android.view.View
-import androidx.core.app.ActivityCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
@@ -15,12 +11,10 @@ import androidx.navigation.ui.setupWithNavController
 import com.jeanpaulo.musiclibrary.app.databinding.ActivityMusicBinding
 import com.jeanpaulo.musiclibrary.commons.base.BaseMvvmActivity
 import com.jeanpaulo.musiclibrary.core.domain.model.Song
-import com.jeanpaulo.musiclibrary.core.presentation.SimpleMusicDetailUIModel
 import com.jeanpaulo.musiclibrary.core.music_player.MPEvents
 import com.jeanpaulo.musiclibrary.core.music_player.MPReceiver
-import com.jeanpaulo.musiclibrary.music.ui.view.MusicDetailActivity
-import com.jeanpaulo.musiclibrary.music.ui.view.bottom_sheet.FullPlayerBottomSheet
-import com.jeanpaulo.musiclibrary.music.ui.view.bottom_sheet.MiniPlayerBottomSheet
+import com.jeanpaulo.musiclibrary.music.ui.bottom_sheet.FullPlayerBottomSheet
+import com.jeanpaulo.musiclibrary.music.ui.bottom_sheet.MiniPlayerBottomSheet
 import com.jeanpaulo.musiclibrary.settings.ui.SettingsActivity
 
 
@@ -40,42 +34,14 @@ class MainActivity : BaseMvvmActivity() {
 
         lifecycle.addObserver(vm)
 
-        setupListeners()
-        setupWidgets()
-        setupBottomsheets()
+        setupNavigation()
+        setupPlayers()
     }
 
-    fun setupListeners() {
-        vm.actMusicFragment.observe(this) { fragmentEnum ->
-            when (fragmentEnum) {
-                MainFragmentEnum.FavoritesFragment -> {}
-                MainFragmentEnum.PlaylistFragment -> {}
-                MainFragmentEnum.SearchFragment -> {}
-            }
-        }
-        vm.state.observe(this) { state ->
-            when (state) {
-                is MusicActivityState.OpenMusicDetail ->
-                    startMusicDetailActivity(state.music)
-
-                else -> {}
-            }
-        }
-    }
-
-    fun startMusicDetailActivity(music: SimpleMusicDetailUIModel) {
-        ActivityCompat.startActivity(
-            this,
-            MusicDetailActivity.newInstance(this, music, false),
-            Bundle.EMPTY
-        )
-    }
-
-    fun setupWidgets() {
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+    private fun setupNavigation() {
+        val navController = (supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+            .navController
 
         binding.navView.setupWithNavController(navController)
         binding.navView.setOnItemSelectedListener { item ->
@@ -95,12 +61,12 @@ class MainActivity : BaseMvvmActivity() {
         }
     }
 
-    fun setupBottomsheets() {
+    private fun setupPlayers() {
         setupMiniPlayer()
         setupFullPlayer()
     }
 
-    fun setupFullPlayer() {
+    private fun setupFullPlayer() {
         fullPlayerDialog = FullPlayerBottomSheet.newInstance(
             binding = binding.fullPlayer,
             listener = object : FullPlayerBottomSheet.FullPlayerBottomSheetListener {
@@ -112,7 +78,7 @@ class MainActivity : BaseMvvmActivity() {
         )
     }
 
-    fun setupMiniPlayer() {
+    private fun setupMiniPlayer() {
         miniPlayerBottomSheet = MiniPlayerBottomSheet.newInstance(
             binding = binding.miniPlayer,
             listener = object : MiniPlayerBottomSheet.MiniPlayerBottomSheetListener {
@@ -121,6 +87,30 @@ class MainActivity : BaseMvvmActivity() {
                 }
             })
     }
+
+    override fun onResume() {
+        super.onResume()
+        setupMusicService()
+    }
+
+    private fun setupMusicService() {
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            musicPlayerReceiver,
+            IntentFilter(MPReceiver.ACTION),
+        )
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(musicPlayerReceiver)
+        super.onPause()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp() ||
+                super.onSupportNavigateUp()
+    }
+
+    // RECEIVER
 
     private val musicPlayerReceiver: MPReceiver<Song> =
         MPReceiver({
@@ -153,36 +143,4 @@ class MainActivity : BaseMvvmActivity() {
                 fullPlayerDialog.updateCounter(counter.toInt())
             }
         })
-
-    private fun setupMusicService() {
-        LocalBroadcastManager.getInstance(this).registerReceiver(
-            musicPlayerReceiver,
-            IntentFilter(MPReceiver.ACTION),
-        )
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setupMusicService()
-    }
-
-    override fun onPause() {
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(musicPlayerReceiver)
-        super.onPause()
-    }
-
-    override fun onCreateContextMenu(
-        menu: ContextMenu?,
-        v: View?,
-        menuInfo: ContextMenu.ContextMenuInfo?
-    ) {
-        //Removed super.onCreateContextMenu to work with context on fragment
-        //REF: https://stackoverflow.com/questions/20825118/inappropriate-context-menu-within-a-fragment
-    }
-
-
-    override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp() ||
-                super.onSupportNavigateUp()
-    }
 }
